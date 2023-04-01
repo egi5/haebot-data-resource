41879352d58050ce299f5814897a67c9734cfcb5
krisna-setyawan <krisn4setyawan@gmail.com>
2023-04-02 05:27:03 +0700

    update migration finance and pembelian

--- app/Database/Migrations/2023-03-10-025659_Finance.php
+++ app/Database/Migrations/2023-03-10-025659_Finance.php
@@ -43,18 +43,16 @@ class Finance extends Migration
         // Transaksi Jurnal
         $fields = [
             'id'                => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
-            'id_akun'           => ['type' => 'int', 'constraint' => 11, 'unsigned' => true],
             'nomor_transaksi'   => ['type' => 'varchar', 'constraint' => 30],
             'referensi'         => ['type' => 'varchar', 'constraint' => 30],
             'tanggal'           => ['type' => 'date'],
             'total_transaksi'   => ['type' => 'int', 'constraint' => 11],
-            'created_at'        => ['type' => 'datetime'],
-            'updated_at'        => ['type' => 'datetime'],
-            'deleted_at'        => ['type' => 'datetime'],
+            'created_at'        => ['type' => 'datetime', 'null' => true],
+            'updated_at'        => ['type' => 'datetime', 'null' => true],
+            'deleted_at'        => ['type' => 'datetime', 'null' => true],
         ];
         $this->forge->addField($fields);
         $this->forge->addKey('id', true);
-        $this->forge->addForeignKey('id_akun', 'akun', 'id', '', 'CASCADE');
         $this->forge->createTable('transaksi_jurnal', true);
 
 
